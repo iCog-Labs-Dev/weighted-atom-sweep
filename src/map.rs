8758cd902f789a9d080cfb3b3c69db6aea28fd49
BlightG <birhane.gulilat@gmail.com>
2025-12-30 12:17:51 +0300

    add new method for weightedMap

--- src/map.rs
+++ src/map.rs
@@ -6,6 +6,16 @@ pub struct WeightedMap<H: AtomHeader> {
     pub inner: Arc<ZipperHeadOwned<H>>,
 }
 
+impl<H:AtomHeader> WeightedMap<H> {
+    
+    pub fn new(map: ZipperHeadOwned<H>) -> Self {
+        
+        Self {
+            inner: Arc::new(map),
+        }
+    }
+}
+
 impl<H> Deref for WeightedMap<H>
 where
     H: AtomHeader,
