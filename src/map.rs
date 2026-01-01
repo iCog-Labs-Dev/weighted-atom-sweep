08cc81fe05601ecb2d327a1eb34246c4c4a00161
BlightG <birhane.gulilat@gmail.com>
2026-01-01 11:26:21 +0300

    feat: create a getter and setter for weighted atom sweep

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,5 @@
 use crate::sweep::AtomHeader;
-use pathmap::zipper::ZipperHeadOwned;
+use pathmap::zipper::{ ZipperCreation, ZipperHeadOwned, ZipperValues, ZipperWriting };
 use std::{ops::Deref, sync::Arc};
 
 pub struct WeightedMap<H: AtomHeader> {
@@ -13,6 +13,27 @@ impl<H:AtomHeader> WeightedMap<H> {
         Self {
             inner: Arc::new(map),
         }
+    }
+
+    pub fn get_val(&self, path: &[u8]) -> Option<H> {
+
+        match self.inner.read_zipper_at_path(path) {
+            Ok(reader) => reader.val().cloned(),
+            Err(_) => None 
+        }
+    }
+
+    pub fn set_val(&self, path: &[u8], val: H) -> () {
+
+        match self.inner.write_zipper_at_exclusive_path(path) {
+            Ok(mut write_zipper) => {
+                write_zipper.set_val(val);
+                self.inner.cleanup_write_zipper(write_zipper);
+            }
+            Err(_) => {
+                return;
+            }
+        };
     }
 }
 
